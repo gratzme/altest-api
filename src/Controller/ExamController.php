7ab5a691ca1876b334bcd97fc4919f7e534a022a
Frederick Gratuito <freddieygratch@gmail.com>
2019-12-02 15:27:27 +0800

    jsonresponse

--- src/Controller/ExamController.php
+++ src/Controller/ExamController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Service\ShortUrls;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -27,14 +27,13 @@ class ExamController extends FOSRestController
      * Lists all urls
      * @Rest\Get("/list")
      * 
-     * @return Response
+     * @return JsonResponse
      */
     public function list()
     {
         $urlList = $this->shortUrls->getUrlList();
-        $response = new Response();
-        $response->setData(json_encode($urlList));
-        $response->headers->set('Content-Type', 'application/json');
+        $response = new JsonResponse();
+        $response->setData($urlList);
         $response->headers->set('Access-Control-Allow-Origin', '*');
         return $response;
     }
